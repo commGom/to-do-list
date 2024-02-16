29cf844990ee5bb5957156b20d688bbf9e44c9f5
commGom <hyunwoo921129@gmail.com>
2024-02-17 00:17:09 +0900

    fixed : alert when clicking with empty content

--- main.js
+++ main.js
@@ -10,48 +10,54 @@
 // 2. true이면 끝난걸로 간주하고 밑줄
 // 3. false 이면 안 끝난걸로 간주하고 그대로
 
-let horizontalUnderLine=document.getElementById("under-line");
-let horizontalMenus=document.querySelectorAll(".menu")
+let horizontalUnderLine = document.getElementById("under-line");
+let horizontalMenus = document.querySelectorAll(".menu")
 console.log(horizontalMenus)
 
-horizontalMenus.forEach((menu)=>menu.addEventListener("click",(e)=>horizontalIndicator(e)));
+horizontalMenus.forEach((menu) => menu.addEventListener("click", (e) => horizontalIndicator(e)));
 
-function horizontalIndicator(e){
-  horizontalUnderLine.style.left=e.currentTarget.offsetLeft+"px";
-  horizontalUnderLine.style.width=e.currentTarget.offsetWidth+"px";
-  horizontalUnderLine.style.top=e.currentTarget.offsetTop+e.currentTarget.offsetHeight+"px";
+function horizontalIndicator(e) {
+  horizontalUnderLine.style.left = e.currentTarget.offsetLeft + "px";
+  horizontalUnderLine.style.width = e.currentTarget.offsetWidth + "px";
+  horizontalUnderLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
 }
 
 let taskInput = document.getElementById("task-input");
 console.log(taskInput)
 
 let addButton = document.getElementById("add-button");
-let tabs=document.querySelectorAll(".task-tabs div");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
 
 addButton.addEventListener("click", addTask);
 
-for(let i=1;i<tabs.length;i++){
-  tabs[i].addEventListener("click",function(event){filter(event)})
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) { filter(event) })
 }
 console.log(tabs);
 function addTask() {
   // task 를 객체로 만들어서 저장 (할일 내용, 완료여부-기본값false), item에 unique한 value(random)로 부여할 것이다.
-  let task={
-    id:generateRandomID(),
-    taskContent:taskInput.value,
-    isComplete:false
+  if (taskInput.value == "") {
+    alert("할 일을 입력해주세요!")
+  } else {
+    let task = {
+      id: generateRandomID(),
+      taskContent: taskInput.value,
+      isComplete: false
+    }
+    taskList.push(task);
+    taskInput.value = "";
   }
-  taskList.push(task);
+
   render();
 }
 
-function render(mode='all') {
+function render(mode = 'all') {
   let resultHTML = ''
-  if(mode=='all'){
+  if (mode == 'all') {
     for (let i = 0; i < taskList.length; i++) {
       const taskContent = taskList[i].taskContent
-      if(taskList[i].isComplete==true){
+      if (taskList[i].isComplete == true) {
         resultHTML += `<div class="task">
         <div class="task-done">${taskContent}</div>
         <div>
@@ -59,7 +65,7 @@ function render(mode='all') {
         <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
         </div>
         </div>`;
-      }else{
+      } else {
         resultHTML += `<div class="task">
         <div>${taskContent}</div>
         <div>
@@ -69,10 +75,10 @@ function render(mode='all') {
         </div>`;
       }
     }
-  }else if(mode=='ongoing'){
+  } else if (mode == 'ongoing') {
     for (let i = 0; i < taskList.length; i++) {
       const taskContent = taskList[i].taskContent
-      if(taskList[i].isComplete==false){
+      if (taskList[i].isComplete == false) {
         resultHTML += `<div class="task">
         <div>${taskContent}</div>
         <div>
@@ -82,10 +88,10 @@ function render(mode='all') {
       </div>`;
       }
     }
-  }else if(mode=='done'){
+  } else if (mode == 'done') {
     for (let i = 0; i < taskList.length; i++) {
       const taskContent = taskList[i].taskContent
-      if(taskList[i].isComplete==true){
+      if (taskList[i].isComplete == true) {
         resultHTML += `<div class="task">
         <div>${taskContent}</div>
         <div>
@@ -96,46 +102,46 @@ function render(mode='all') {
       }
     }
   }
-    document.getElementById('task-board').innerHTML = resultHTML;
+  document.getElementById('task-board').innerHTML = resultHTML;
 }
 
-function toggleComplete(id){
+function toggleComplete(id) {
   console.log("Check 됐음!")
-  console.log("id:",id)
-  for(let i=0;i<taskList.length;i++){
-    if(taskList[i].id==id){
+  console.log("id:", id)
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
       // taskList[i].isComplete=taskList[i].isComplete==true?false:true;
-      taskList[i].isComplete=!taskList[i].isComplete;
+      taskList[i].isComplete = !taskList[i].isComplete;
       break;
     }
   }
   render(mode)
 }
 
-function generateRandomID(){
+function generateRandomID() {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-function deleteTask(id){
+function deleteTask(id) {
   console.log("삭제하자!")
-  for(let i=0;i<taskList.length;i++){
-    if(taskList[i].id==id){
-      taskList.splice(i,1);
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
       break;
     }
   }
   render(mode)
 }
 
-let mode="all"
+let mode = "all"
 
-function filter(e){
+function filter(e) {
   // 누구를 클릭했는지에 대한 event에 대한 정보를 매개변수로 받아옴
-  console.log("filter",e.target.id);
-  mode=e.target.id
-  let filteredResultHTML=""
-    // mode=='all'이면 전체리스트 보여준다
-    // mode=='ongoing' 이면, isComplete가 false인 item을 보여준다.
-    // mode=='done'이면 isComplete가 true인 item을 보여준다.
-    render(mode)
+  console.log("filter", e.target.id);
+  mode = e.target.id
+  let filteredResultHTML = ""
+  // mode=='all'이면 전체리스트 보여준다
+  // mode=='ongoing' 이면, isComplete가 false인 item을 보여준다.
+  // mode=='done'이면 isComplete가 true인 item을 보여준다.
+  render(mode)
 }
